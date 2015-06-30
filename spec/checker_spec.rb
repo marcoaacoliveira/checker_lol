a67ccf21f41f93d06d5ed202e5ec83216e3312af
Marco <marcooliveira@infojr.com.br>
2015-06-30 12:36:55 -0300

    Spec para o checker agora funcionando corretamente

--- spec/checker_spec.rb
+++ spec/checker_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Checker do
     	@checker = Checker.new "e5fa8938-e73e-4d45-9398-0e7d56f202b4"
 	end
 	it "have a api_key" do
-		expect(@checker.get_id("AdrianLion")).to eq 418428 
+		 expect(@checker.api_key).to eq "e5fa8938-e73e-4d45-9398-0e7d56f202b4"
 	end
 	it "get a summoner's name by id" do
-
+		expect(@checker.get_id("AdrianLion")).to eq 418428
 	end
 end
